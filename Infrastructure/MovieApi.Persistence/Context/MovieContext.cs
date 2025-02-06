8192c1cf9f9263b4f1465f07439fd4f59be63ae9
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2025-02-06 15:59:23 +0300

    ...

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MEHMET; initial Catalog=ApiMovieDb; integrated Security=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
